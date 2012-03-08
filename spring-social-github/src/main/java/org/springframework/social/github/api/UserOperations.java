178a8891eaa3c60cc13d184ad2fce9ba15868b1d
Willie Wheeler <willie.wheeler@gmail.com>
2012-03-08 00:00:22 -0700

    Supporting commits

--- spring-social-github/src/main/java/org/springframework/social/github/api/UserOperations.java
+++ spring-social-github/src/main/java/org/springframework/social/github/api/UserOperations.java
@@ -51,7 +51,7 @@ public interface UserOperations {
 	 * @param user GitHub user
 	 * @return list of followers
 	 */
-	public List<GitHubUser> getFollowers(String user);
+	List<GitHubUser> getFollowers(String user);
 	
 	/**
 	 * Public operation to return the users that a given user is following.
@@ -59,5 +59,5 @@ public interface UserOperations {
 	 * @param user GitHub user
 	 * @return list of users the given user is following
 	 */
-	public List<GitHubUser> getFollowing(String user);
+	List<GitHubUser> getFollowing(String user);
 }
