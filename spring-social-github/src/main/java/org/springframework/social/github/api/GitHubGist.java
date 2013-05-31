fdfb2635c06568913d4616d301422b52299c388e
Craig Walls <cwalls@vmware.com>
2013-05-31 16:00:53 -0500

    Update to Jackson 2. SOCIAL-362

--- spring-social-github/src/main/java/org/springframework/social/github/api/GitHubGist.java
+++ spring-social-github/src/main/java/org/springframework/social/github/api/GitHubGist.java
@@ -19,8 +19,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
 
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.codehaus.jackson.annotate.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * A GitHub gist.
